4fd6fed11ab6cc9411d142dbd04eb1bc11a7ed21
jovenloba <117356497+jovenloba@users.noreply.github.com>
2025-07-15 20:24:11 +0800

    first commit

---
+++ clientvite/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './assets/css/generalcss.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='main-container'>
+      <h3 className='container-title'>Title</h3>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
